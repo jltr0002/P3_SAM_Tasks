10b4db588b7a51e4937c05fd858be22fdfd06184
jltr0002 <jltr0002@red.ujaen.es>
2025-05-30 02:56:16 +0200

    Task 7 working with chat perfectly

--- Task_7_react-webrtc-masterP3/src/index.js
+++ Task_7_react-webrtc-masterP3/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import process from 'process';
+import { Buffer } from 'buffer';
+
+window.process = process;   // make the global
+window.Buffer  = Buffer;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
